1adea1952decbf4cd6e4ac8e231dbcd79dbd647d
keeykeey <sh1n_fuj1to@icloud.com>
2023-02-26 18:57:31 +0900

    [update]PaintPalletに画像出力機能を追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,37 +12,19 @@
       :color = "fontColor"
       :fontSize  = "fontSize"
     />
-    <Radio tag=出力形式
-      :radioTitle = "radioTitle"
-      :radioVal = "imageFormatKey"
-      :setVal = "setImageFormat"
-      :keys = "keyVals"
-      :vals = "radioVals"
-      :labels = "radioLabels"
-    ></Radio>
-    <Button tag = 画像出力ボタン
-      :btnLabel = "buttonLabel"
-      :pushBtn = "outputPicture"
-    >
-    </Button>
   </div>
 </template>
 
 <script>
 import PaintPallet from './components/PaintPallet'
 import Selector from "./components/Selector"
-import Radio from "./components/Radio"
-import Button from "./components/Button"
 const DEFAULT_FONT_SIZE = "3"
-const DEFAULT_IMG_FMT_KEY = "0"
 
 export default {
   name: 'App',
   components: { 
     PaintPallet,
     Selector,
-    Radio,
-    Button,
   },
   data: function(){
     return {
@@ -50,35 +32,16 @@ export default {
       fontColor: "#40a174",
       fontSize: DEFAULT_FONT_SIZE,
       fontSizeList: [1,2,3,4,5,6,7,8,9,10],
-      
-      // Radio -> image format
-      radioTitle: "出力形式",
-      imageFormatKey: DEFAULT_IMG_FMT_KEY,
-      keyVals: ["0", "1", "2"],
-      radioVals: ["0", "1", "2"],
-      radioLabels: ["png", "jpg", "svg"],
-
-      // Button -> to output picture
-      buttonLabel: "画像出力",
     }
   },
   methods: {
+    // Selector -> fontsize
     setColor(color) {
       this.color = color;
     },
     setFontSize(size) {
       this.fontSize = String(size);
     },
-    setImageFormat(val) {
-      this.imageFormatKey = String(val);
-    },
-    outputPicture(){
-      console.log("output picture in format type : ",this.imageFormatKey);
-      /**
-       * TODO
-       * https://note.affi-sapo-sv.com/js-canvas-download.php
-       */
-    }
   }
 }
 </script>
--- frontend/src/components/PaintPallet.vue
+++ frontend/src/components/PaintPallet.vue
@@ -10,12 +10,34 @@
             @mouseup   = "changeMode"
          ></canvas>
     </div>
+    <Radio tag=出力形式
+      :radioTitle = "radioTitle"
+      :radioVal = "imageFormatKey"
+      :setVal = "setImageFormat"
+      :keys = "keyVals"
+      :vals = "radioVals"
+      :labels = "radioLabels"
+    ></Radio>
+    <Button tag = 画像出力ボタン
+      :btnLabel = "buttonLabel"
+      :pushBtn = "outputPicture"
+    >
+    </Button>
   </div>
 </template>
 
 <script>
+import Radio from "./Radio"
+import Button from "./Button"
+const DEFAULT_IMG_FMT_KEY = "0"
+const COMPRESSION_RATIO = 1
+
 export default {
     name: "A_PaintPallet",
+    components: {
+        Radio,
+        Button,
+    },
     props: {
         color     : String,
         fontSize  : String,
@@ -23,15 +45,26 @@ export default {
     data: function(){
         return {
             is_drawing_mode: false,
+
+            // Radio -> image format
+            radioTitle: "出力形式",
+            imageFormatKey: DEFAULT_IMG_FMT_KEY,
+            keyVals: ["0", "1", "2"],
+            radioVals: ["0", "1", "2"],
+            radioLabels: ["png", "jpg", "svg"],
+
+            // Button -> to output picture
+            buttonLabel: "画像出力",
         }
     },
-    mounted: function(){
+    mounted: function() {
         this.canvas  = this.$refs.canvas;
         this.context = this.canvas.getContext("2d");
         this.rect    = this.canvas.getBoundingClientRect();
     },
     methods: {
-        draw(e){
+        // PaintPallet itself
+        draw(e) {
             switch (this.is_drawing_mode){
                 case false:
                     break;
@@ -50,16 +83,39 @@ export default {
                     break;
             }
         },
-        changeMode(){
+        changeMode() {
             this.is_drawing_mode = !this.is_drawing_mode;
+        },
+
+        // Radio -> image format
+        setImageFormat(val) {
+            this.imageFormatKey = String(val);
+        },
+
+        // Button -> to output picture
+        outputPicture() {
+            console.log("this.imageFormatKey :",this.imageFormatKey)
+            /**
+            * https://note.affi-sapo-sv.com/js-canvas-download.php
+            * https://developer.mozilla.org/ja/docs/Web/API/HTMLCanvasElement/toBlob
+            * 擬似的にaタグ要素とそのurlを作成し、urlをクリックすることで画像をダウンロードすることにする。
+            */
+            const mapper = {
+                "0": "image/png",
+                "1": "image/jpeg",
+            }
+            const format = mapper[this.imageFormatKey];
+
+            this.canvas.toBlob(function(blob) {
+                const a = document.createElement("a");
+                a.href = URL.createObjectURL(blob);
+                a.download = "download";
+                a.click();
+            },format,COMPRESSION_RATIO)
         }
     }
 }
-
-
 </script>
-
-
 
 <style scoped>
 canvas{
