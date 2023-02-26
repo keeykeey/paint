c9f06569d8f5451deefb036d25d2a627866ed571
keeykeey <sh1n_fuj1to@icloud.com>
2023-02-26 16:40:06 +0900

    [update]Button作成による微修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,6 +20,11 @@
       :vals = "radioVals"
       :labels = "radioLabels"
     ></Radio>
+    <Button tag = 画像出力ボタン
+      :btnLabel = "buttonLabel"
+      :pushBtn = "outputPicture"
+    >
+    </Button>
   </div>
 </template>
 
@@ -27,6 +32,7 @@
 import PaintPallet from './components/PaintPallet'
 import Selector from "./components/Selector"
 import Radio from "./components/Radio"
+import Button from "./components/Button"
 const DEFAULT_FONT_SIZE = "3"
 const DEFAULT_IMG_FMT_KEY = "0"
 
@@ -36,27 +42,24 @@ export default {
     PaintPallet,
     Selector,
     Radio,
+    Button,
   },
   data: function(){
-    return{
-      // selector -> fontsize
+    return {
+      // Selector -> fontsize
       fontColor: "#40a174",
       fontSize: DEFAULT_FONT_SIZE,
       fontSizeList: [1,2,3,4,5,6,7,8,9,10],
       
-      // radio -> image format
-      tag: "画像出力形式",
+      // Radio -> image format
       radioTitle: "出力形式",
       imageFormatKey: DEFAULT_IMG_FMT_KEY,
       keyVals: ["0", "1", "2"],
       radioVals: ["0", "1", "2"],
       radioLabels: ["png", "jpg", "svg"],
-    }
-  },
-  watch: {
-    imageFormatKey: function(val) {
-      // TODO //
-      console.log("watch val",val);
+
+      // Button -> to output picture
+      buttonLabel: "画像出力",
     }
   },
   methods: {
@@ -68,6 +71,13 @@ export default {
     },
     setImageFormat(val) {
       this.imageFormatKey = String(val);
+    },
+    outputPicture(){
+      console.log("output picture in format type : ",this.imageFormatKey);
+      /**
+       * TODO
+       * https://note.affi-sapo-sv.com/js-canvas-download.php
+       */
     }
   }
 }
